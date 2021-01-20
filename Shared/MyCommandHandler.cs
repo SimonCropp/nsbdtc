f8633585f59bf4acd41255868bd0527196d613bf
Simon Cropp <simon.cropp@gmail.com>
2021-01-20 11:10:45 +1100

    .

--- Shared/MyCommandHandler.cs
+++ Shared/MyCommandHandler.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 using NServiceBus;
 using NServiceBus.Logging;
 
@@ -8,6 +10,18 @@ public class MyCommandHandler : IHandleMessages<MyCommand>
 
     public Task Handle(MyCommand commandMessage, IMessageHandlerContext context)
     {
+        using (var connection = new SqlConnection("Data Source=.;Database=OtherDb;Integrated Security=True;Max Pool Size=100"))
+        {
+            connection.Open();
+            using (var command = connection.CreateCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = "INSERT INTO Table_1 (id) VALUES (@val1)";
+                command.Parameters.AddWithValue("@val1", Guid.NewGuid());
+                command.ExecuteNonQuery();
+            }
+        }
+
         log.Info($"Hello from {nameof(MyCommandHandler)}");
         return Task.CompletedTask;
     }
