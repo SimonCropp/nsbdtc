a7c43f6f23b19eb53d7a6e006feac20e4dfef686
Simon Cropp <simon.cropp@gmail.com>
2021-01-20 14:52:12 +1100

    .

--- OnlyTransport/Program.cs
+++ OnlyTransport/Program.cs
@@ -16,27 +16,8 @@ class Program
         configuration.PurgeOnStartup(true);
         var endpointInstance = await Endpoint.Start(configuration)
             .ConfigureAwait(false);
-        await SendMessages(endpointInstance);
+        await MessageSender.SendMessages(endpointInstance);
         await endpointInstance.Stop()
             .ConfigureAwait(false);
-    }
-
-    static async Task SendMessages(IMessageSession messageSession)
-    {
-        Console.WriteLine("Press [c] to send a command. Press [Esc] to exit.");
-        while (true)
-        {
-            var input = Console.ReadKey();
-            Console.WriteLine();
-
-            switch (input.Key)
-            {
-                case ConsoleKey.C:
-                    await messageSession.SendLocal(new MyCommand());
-                    break;
-                case ConsoleKey.Escape:
-                    return;
-            }
-        }
     }
 }
---
+++ Shared/MessageSender.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+public class MessageSender
+{
+    public static async Task SendMessages(IMessageSession messageSession)
+    {
+        Console.WriteLine("Press [c] to send a command. Press [Esc] to exit.");
+        while (true)
+        {
+            var input = Console.ReadKey();
+            Console.WriteLine();
+
+            switch (input.Key)
+            {
+                case ConsoleKey.C:
+                    await messageSession.SendLocal(new MyCommand());
+                    break;
+                case ConsoleKey.Escape:
+                    return;
+            }
+        }
+    }
+}
